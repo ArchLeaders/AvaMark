b0f0be465d9a3c90e5ebe690599f02814c6210a8
ArchLeaders <archleadership28@gmail.com>
2025-01-25 13:25:23 -0800

    Update demo app

--- Samples/AvaMark.Demo/MainWindow.axaml.cs
+++ Samples/AvaMark.Demo/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System.IO;
 using Avalonia.Controls;
+using Markdig;
 
 namespace AvaMark.Demo;
 
@@ -7,5 +9,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        string md = File.ReadAllText("Assets\\Sample.md");
+        
+        MdView.Markdown = md;
+        HtmlView.Text = Markdown.ToHtml(md);
     }
+    
 }
--- Samples/AvaMark.Demo/Program.cs
+++ Samples/AvaMark.Demo/Program.cs
@@ -1,5 +1,8 @@
-﻿using Avalonia;
-using System;
+﻿using System;
+using System.IO;
+using Avalonia;
+using Markdig;
+using Markdig.Renderers;
 
 namespace AvaMark.Demo;
 
@@ -9,8 +12,16 @@ internal class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // using MemoryStream ms = new();
+        // using TextWriter writer = new StreamWriter(ms);
+        // HtmlRenderer renderer = new(writer);
+        // Markdown.Convert("**Test**", renderer);
+        
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
