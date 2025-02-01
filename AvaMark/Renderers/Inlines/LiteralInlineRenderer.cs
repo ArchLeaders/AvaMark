4e777a992fdc1c9fde2e5241fbcd2f8a8221979f
ArchLeaders <archleadership28@gmail.com>
2025-01-31 17:00:53 -0800

    Classify inline literals

--- AvaMark/Renderers/Inlines/LiteralInlineRenderer.cs
+++ AvaMark/Renderers/Inlines/LiteralInlineRenderer.cs
@@ -5,10 +5,16 @@ namespace AvaMark.Renderers.Inlines;
 
 internal class LiteralInlineRenderer : AvaloniaObjectRenderer<LiteralInline>
 {
+    private const string CLASS = "p";
+    
     protected override void Write(AvaloniaMarkdownRenderer renderer, LiteralInline obj)
     {
+        renderer.PushStyle(CLASS);
+        
         renderer.WriteInline(new Run {
             Text = obj.ToString()
         });
+        
+        renderer.DropStyle();
     }
 }
